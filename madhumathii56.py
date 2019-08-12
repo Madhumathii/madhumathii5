d5cddf6994f3e3006094888fcefc31dcf1d4ff49
Madhumathii <52281595+Madhumathii@users.noreply.github.com>
2019-08-12 14:16:59 +0530

    Create madhumathii56.py

---
+++ madhumathii56.py
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int num,m =1,n=6;
+    
+    for (num = 2; num <= n; num++)
+        {
+               if (num % 2 == 1)
+                  printf ("%d ", num);
+         }
+}
